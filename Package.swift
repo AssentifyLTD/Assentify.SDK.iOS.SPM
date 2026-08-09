7ec2e85bc60bdadc879bd296f8cea24f5a8e64da
TariQ <talsakran@assentify.com>
2026-08-09 11:56:07 +0300

    TF Lite Fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(url: "https://github.com/bugsnag/bugsnag-cocoa-performance.git", from: "2.0.0"),
 
         // Unofficial SPM
-        .package(url: "https://github.com/kewlbear/TensorFlowLiteSwift.git", exact: "2.6.0"),
+        .package(url: "https://github.com/kewlbear/TensorFlowLiteSwift.git", revision: "412bd55"),
     ],
     targets: [
         .target(
